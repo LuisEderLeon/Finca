84b134be085fbc1020f860639b904d20d6e1d108
LuisEderLeon <luiseder.leonvesga@gmail.com>
2024-10-18 16:55:08 -0500

    feat :rocket: added new tables

--- ddl.sql
+++ ddl.sql
@@ -1,12 +1,43 @@
-create table tipoProducto (
+create table tiposProducto (
     id int primary key auto_increment,
     nombre varchar(50) unique
-)
+);
 
 create table productos (
     id int primary key auto_increment,
     nombre varchar(50) unique,
     precio double,
     tipo int,
+    unidades enum("kilogramos","litros","unidades")
     foreign key (tipo) references tipoProducto (id)
+);
+
+create table inventarios (
+    id int primary key auto_increment,
+    idProducto int,
+    estado enum("stock","venta"),
+    fechaIngreso datetime,
+    cantidad double,
+    foreign key (idProducto) references productos (id)
+);
+
+create table maquinarias (
+    id int primary key auto_increment,
+    marca varchar(50),
+    modelo varchar(50),
+    estado ("stock","uso"),
+    fechaCompra datetime
+);
+
+create table funciones (
+    id int primary key auto_increment,
+    nombre varchar(50)unique
+);
+
+create table empleados (
+    id int primary key auto_increment,
+    nombre varchar (50),
+    fechaNacimiento datetime,
+    funcion int,
+
 )
