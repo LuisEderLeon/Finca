d2f807e455f3d4f7e60a1d169da93f180389d9ac
JoseDanielCM <jodacarmon@gmail.com>
2024-10-19 08:33:49 -0500

    feat: :sparkles: agregar tablas ddl

--- ddl.sql
+++ ddl.sql
@@ -40,6 +40,7 @@ create table empleados (
     fechaNacimiento datetime,
     idFuncion int,
     telefono varchar(20),
+    fechaContratacion date,
     foreign key (funcion) references funciones (id)
 );
 
@@ -53,12 +54,83 @@ create table alimentos (
     nombre varchar(50)
 )
 
-create table animales (
+create table animal  (
     id int primary key auto_increment,
     idEspecie int,
     fechaNacimiento datetime,
     idAlimento int,
     cantidadAlimento int,
+    estadoSalud enum("bueno","regular","malo"),
     foreign key (idEspecie) references especies (id),
     foreign key (idAlimento) references alimentos (id)
 )
+
+CREATE TABLE proveedor (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,  
+    telefono VARCHAR(20)       
+);
+
+CREATE TABLE compra (
+    id INT AUTO_INCREMENT PRIMARY KEY,  
+    fecha DATE NOT NULL,                
+    idProveedor INT NOT NULL,           
+    total DECIMAL(10, 2) NOT NULL,      
+    FOREIGN KEY (idProveedor) REFERENCES proveedor(id) 
+);
+
+CREATE TABLE compraAnimal (
+    idCompra INT NOT NULL,
+    idAnimal INT NOT NULL,
+    cantidad INT NOT NULL,
+    subtotal DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (idCompra, idAnimal),
+    FOREIGN KEY (idCompra) REFERENCES compra(id),
+    FOREIGN KEY (idAnimal) REFERENCES animal(id)
+);  
+
+CREATE TABLE alimento (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100),
+    stock INT,
+);  
+
+CREATE TABLE compraAlimento (
+    idCompra INT,
+    idAlimento INT,
+    cantidad INT,
+    subtotal DECIMAL(10, 2),
+    PRIMARY KEY (idCompra, idAlimento),
+    FOREIGN KEY (idCompra) REFERENCES compra(id),
+    FOREIGN KEY (idAlimento) REFERENCES alimento(id)
+);
+
+CREATE TABLE produccion (
+    idAnimal INT,
+    idProducto INT,
+    cantidad DECIMAL(10, 2),
+    unidad enum("kg","litros","unidades"),
+    idEmpleado INT,
+    PRIMARY KEY (idAnimal, idProducto, idEmpleado),
+    FOREIGN KEY (idProducto) REFERENCES productos(id), 
+    FOREIGN KEY (idEmpleado) REFERENCES empleado(id),
+    FOREIGN KEY (idAnimal) REFERENCES animal(id)
+);
+-- ---------------------------------------------------------------------------------
+CREATE TABLE ventas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fecha DATE NOT NULL,
+    idCliente INT NOT NULL,
+    total DECIMAL(10, 2),
+    FOREIGN KEY (idCliente) REFERENCES clientes(id)
+);
+
+CREATE TABLE detallesVenta (
+    idVenta INT,
+    idProducto INT,
+    cantidad INT,
+    subtotal DECIMAL(10, 2),
+    PRIMARY KEY (idVenta, idProducto),
+    FOREIGN KEY (idVenta) REFERENCES ventas(id),
+    FOREIGN KEY (idProducto) REFERENCES productos(id)
+);
